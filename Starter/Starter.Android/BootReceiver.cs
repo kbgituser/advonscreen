cef4f3cc62c248886bc6d6fe2ec29fe09c282fdf
kbgituser <kanatbat@mail.ru>
2021-01-07 15:22:37 +0600

    starter

---
+++ Starter/Starter.Android/BootReceiver.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace Starter.Droid
+{
+    //[BroadcastReceiver]
+    //[IntentFilter(new[] { Android.Content.Intent.ActionBootCompleted })]
+    //[IntentFilter(new[] { Intent.ActionBootCompleted }, Priority = (int)IntentFilterPriority.LowPriority)]
+    [BroadcastReceiver(Enabled = true, Exported = true, DirectBootAware = true)]
+    [IntentFilter(new string[] {
+    Intent.ActionBootCompleted, Intent.ActionLockedBootCompleted, "android.intent.action.QUICKBOOT_POWERON"
+})]
+    public class BootReceiver : BroadcastReceiver
+    {
+        public override void OnReceive(Context context, Intent intent)
+        {
+
+            if (intent.Action == Intent.ActionBootCompleted)
+            {                
+                Intent i = new Intent(context, typeof(MainActivity));
+                i.AddFlags(ActivityFlags.NewTask);
+                context.StartActivity(i);
+            }
+
+            
+        }
+    }
+}
---
+++ Starter/Starter.Android/CustomWebViewRenderer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Starter;
+using Starter.Droid;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(CustomWebView), typeof(CustomWebViewRenderer))]
+namespace Starter.Droid
+{
+    
+    public class CustomWebViewRenderer : WebViewRenderer
+    {
+        public CustomWebViewRenderer(Context context) : base(context)
+        {
+
+        }
+        protected override void OnElementChanged(ElementChangedEventArgs<WebView> e)
+        {
+            base.OnElementChanged(e);
+            global::Android.Webkit.WebView.SetWebContentsDebuggingEnabled(true);            
+            Control.Settings.JavaScriptEnabled = true;
+        }
+    }
+}
