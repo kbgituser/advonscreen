18e2d7be02834617c1f01e31a6a42a7a58f22d07
kbgituser <kanatbat@mail.ru>
2021-01-07 15:21:54 +0600

    screen checker

---
+++ ScreenChecker/ScreenChecker/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ScreenChecker
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();            
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            viewSource.Html =  "Ширина - "+webView.Width + "; Высота - " + webView.Height+";";;
+            webView.Source = viewSource;
+            WaitAndExecute();
+            //Task.Run(()=> WaitAndExecute());
+        }
+
+        protected async Task WaitAndExecute()
+        {
+            await Task.Delay(3000);
+            viewSource.Html = "Ширина - " + webView.Width + "; Высота - " + webView.Height + ";"; ;
+            webView.Source = viewSource;
+        }
+    }
+}
