67da643c2162d9d0ba946bbcf7f782a80a7a6062
DESKTOP-EK9MK86\user <kanatbat@mail.ru>
2021-04-04 16:39:23 +0600

    инструкция

--- AdvScreen/Controllers/HomeController.cs
+++ AdvScreen/Controllers/HomeController.cs
@@ -34,6 +34,20 @@ namespace AdvScreen.Controllers
             return View();
         }
 
+        //[HttpGet("download")]
+        public IActionResult InstructionsDownload([FromQuery] string link)
+        {
+            var fileName = "Инструкция по работе с REDI.docx";
+            var net = new System.Net.WebClient();
+            //var data = net.DownloadData(fileName);
+            //var content = new System.IO.MemoryStream(data);
+            var contentType = "APPLICATION/octet-stream";
+            
+            var filePath = fileName;
+            byte[] fileBytes = System.IO.File.ReadAllBytes(filePath);
+            return File(fileBytes, contentType, fileName);
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
